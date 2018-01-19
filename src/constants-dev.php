ddfff65c39c54a55dff1532d36367d4b207c95e5
Konstantinas <konstantinast@gmail.com>
2018-01-19 11:54:23 +0200

    https://github.com/konstantinast/fullstack-party/issues/15

---
+++ src/constants-dev.php
@@ -0,0 +1,7 @@
+<?php
+
+// @TODO: remove once visitor is able to authorize using Github
+define('GITHUB_USERNAME', 'KnpLabs');
+define('GITHUB_REPO_NAME', 'php-github-api');
+define('ISSUE_LIST_PER_PAGE_LIMIT', 4);
+define('ISSUE_COMMENT_LIST_PER_PAGE_LIMIT', 5);
